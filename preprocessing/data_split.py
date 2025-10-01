3c2e1a749093285eb6f9d334abdd64c4ab7fe0f3
Albin Shabu <albinshabu960@gmail.com>
2025-10-01 19:02:15 +0530

    added val ratio in data split.py and added different preprocessing for LR and tree models

--- preprocessing/data_split.py
+++ preprocessing/data_split.py
@@ -43,13 +43,16 @@ class DataSplit(DataSplit):
 
             X = data.drop(columns=['Churn', 'customerID', 'gender', 'PhoneService'])
             y = (data['Churn']=='Yes').astype(int)
+            
+            test_size = 0.15
+            val_ratio = test_size / (1 - test_size)
 
             X_temp, X_test, y_temp, y_test = train_test_split(
-                X, y, test_size=0.20, stratify=y, random_state=42
+                X, y, test_size=test_size, stratify=y, random_state=42
                 )
             
             X_train, X_val, y_train, y_val = train_test_split(
-                X_temp, y_temp, test_size=0.25, stratify=y_temp, random_state=42
+                X_temp, y_temp, test_size=val_ratio, stratify=y_temp, random_state=42
             )
 
             return X_train, X_val, X_test, y_train, y_val, y_test
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, FunctionTransformer, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import pandas as pd
@@ -9,14 +9,35 @@ class PreprocessSetup:
 
     def __init__(self, data: pd.DataFrame, numeric_features: list[str],
                 cat_bin_features: list[str]):
-        """Initialize the PreprocessSetup class"""
+
+        """
+        Initialize the PreprocessSetup class
+        
+        Args:
+            data: pd.DataFrame
+            numeric_features: list[str]
+            cat_bin_features: list[str]
+        
+        Returns:
+            None
+        """
+
         self.data = data
         self.numeric_features = numeric_features
         self.cat_bin_features = cat_bin_features
         self.preprocessor = None
 
-    def preprocess(self):
-        """Handles preprocessing pipelines and composing into a single Transformer"""
+    def preprocess(self, model_type: str):
+
+        """
+        Handles preprocessing pipelines and composing into a single Transformer
+
+        Args:
+            model_type (str): The type of model to preprocess for
+        
+        Returns:
+            self.preprocessor (ColumnTransformer): The preprocessor pipeline
+        """
 
         try:
 
@@ -29,12 +50,23 @@ class PreprocessSetup:
                 steps=[
                     ('OHE', OneHotEncoder(drop='first', handle_unknown='ignore'))
                 ])
+                                    
+            
+            if model_type == "lr":
 
-            self.preprocessor = ColumnTransformer(
-                transformers=[
-                    ('num', numeric_transformer, self.numeric_features),
-                    ('cat', cat_bin_transformer, self.cat_bin_features)
-                ])
+                self.preprocessor = ColumnTransformer(
+                    transformers=[
+                        ('num', numeric_transformer, self.numeric_features),
+                        ('cat', cat_bin_transformer, self.cat_bin_features)
+                    ])
+
+            elif model_type == "tree":
+
+                self.preprocessor = ColumnTransformer(
+                    transformers=[
+                        ('num', 'passthrough', self.numeric_features),
+                        ('cat', cat_bin_transformer, self.cat_bin_features)
+                    ])
             
             return self.preprocessor
         
@@ -43,11 +75,30 @@ class PreprocessSetup:
             raise e
     
     def fit_transform(self, split: pd.DataFrame):
-        """Fit and transform the data split using preprocessor"""
+
+        """
+        Fit and transform the data split using preprocessor
+        
+        Args: 
+            split (pd.DataFrame): The data split to fit and transform
+
+        Returns:
+            np.ndarray: The transformed data
+        """
         
         return self.preprocessor.fit_transform(split)
 
     def transform(self, split: pd.DataFrame):
-        """Transform the data split using preprocessor"""
+
+        """
+        Transform the data split using preprocessor
+
+        Args:
+            split (pd.DataFrame): The data split to transform
+
+        Returns:
+            np.ndarray: The transformed data
+
+        """
 
         return self.preprocessor.transform(split)
