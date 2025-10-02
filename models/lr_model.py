44e6c0829e7047c3cb2b7b4518e1e9d8ea2b4102
Albin Shabu <albinshabu960@gmail.com>
2025-10-02 15:32:42 +0530

    Structured the model pipeline in GS CV and added RS CV attribute

--- models/lr_model.py
+++ models/lr_model.py
@@ -23,9 +23,10 @@ class LRModel:
         self.categorical_features = categorical_features
     
     
-    def gridsearch_lr(self, cv, params: dict, model_type: str,
+    def gridsearch(self, cv, params: dict, model_type: str,
                      scoring: str='average_precision',
-                      n_jobs: int=-1):
+                      n_jobs: int=-1, smote: bool=False
+                      ):
         
         """
         The GridSearch Hyperparameter CV method for fitting the
@@ -33,11 +34,14 @@ class LRModel:
         
         Args:
             params (dict): The parameters for hyperparameter tuning
+            model_type (str): Type of model
             scoring (str): The scoring metric. Defaults to average precision
             cv (int): The cross validation folds. Defaults to 5
             n_jobs(int): Parallel processing. Defaults to -1
+            smote (bool): smote pipeline to use. Defaults to False 
             
         Returns:
+            GridSearchCV model
         
         """
         
@@ -48,20 +52,72 @@ class LRModel:
                 cat_bin_features=self.categorical_features
                 )
             
-            
-            
-            model_pipe = preprocess.model_pipeline(model_type=model_type)
+            model_pipe = preprocess.model_pipeline(model_type=model_type,
+                                                       smote=smote)
             
             gridsearch_cv = GridSearchCV(
-                    model_pipe, params, cv=cv,
-                    scoring=scoring, n_jobs=n_jobs
-                    )
+                        model_pipe, params, cv=cv,
+                        scoring=scoring, n_jobs=n_jobs,
+                        refit=True
+                        )
 
             return gridsearch_cv
         
         except Exception as e:
             print("Error occured during building grid search CV as :", str(e))
             raise e
+
+    
+    def randomizedsearch(
+        self, cv, n_iters: int,
+        params: dict, scoring: str, 
+        n_jobs: int, smote: bool, 
+        model_type: str, random_state: int=42, 
+        ):
+        
+        """
+        Perform the randomized search with the input parameters
+        
+        Args:
+            cv: Integer or can use StratifiedKFold for better internal splits
+            n_iters (int): The number of iterations to perform
+            params (dict): The parameters for random search hyperparameter tuning
+            scoring (str): The scoring method 
+            model_type (str): Type of model
+            smote (bool): The SMOTE method if want
+            n_jobs (int): The parallel processing.
+            random_state (int): Random state for reproducability. defaults to 42
+        
+        Returns:
+            RandomizedSearch Cv Model
+
+        """
+        
+        try:
+            preprocess = PreprocessSetup(
+                    data=self.data, numeric_features=self.numeric_features,
+                    cat_bin_features=self.categorical_features
+                    )
+            
+            model_pipe = preprocess.model_pipeline(
+                model_type=model_type, smote=smote
+                )
+
+            randomized_search = RandomizedSearchCV(
+                model_pipe, params, n_iter=n_iters,
+                cv=cv, scoring=scoring, random_state=random_state,
+                n_jobs=n_jobs, refit=True
+            )
+
+            return randomized_search
+
+        except Exception as e:
+            print("Error occured during randomized search cv as :", str(e))
+        
+
+
+        
+
 
 
     
